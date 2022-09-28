0044d574c0fb26664b53cfe4d86a3cff8d55e288
Leonomics <leocv97@gmail.com>
2022-09-28 15:01:59 +0200

    correct objects' names

--- db/products.php
+++ db/products.php
@@ -4,8 +4,8 @@ include __DIR__ . "/../Product.php";
 include __DIR__ . "/../PhysicalProduct.php";
 include __DIR__ . "/../Food.php";
 
-$crocchetteGatti = new Food("crocchette per cani", null, "pacco di crocchette per cani", "Royal Canin", 20, 1, 5, "dog", false);
-$crocchetteCani = new Food("crocchette per gatti", null, "pacco di crocchette per gatti", "Royal Canin", 20, 1, 5, "cat", false);
+$crocchetteCani = new Food("crocchette per cani", null, "pacco di crocchette per cani", "Royal Canin", 20, 1, 5, "dog", false);
+$crocchetteGatti = new Food("crocchette per gatti", null, "pacco di crocchette per gatti", "Royal Canin", 20, 1, 5, "cat", false);
 $dogHouse = new Food("cuccia per cani", null, "cuccia per cani da esterni", "Doghouse inc.", 100, 15 , 50, "dog", true);
 
 
